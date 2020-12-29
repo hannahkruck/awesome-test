523d9a31348c5e35a2e1e5526cb11a9968289240
hannahkruck <73877879+hannahkruck@users.noreply.github.com>
2020-12-29 18:53:45 +0100

    Update detail.py

--- src/pages/gallery/detail.py
+++ src/pages/gallery/detail.py
@@ -11,17 +11,12 @@ import numpy as np
 
 # Get an instance of a logger
 logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
-
-
-
-
 def write():
     """Writes content to the app"""
     #ast.shared.components.title_awesome("Detail")      # Titel Awesome_Streamlit
     
     # Page title
     st.title("Detailed view")
-#   st.header('Hier kann ein Text rein')
     
     # read CSV
     # CSV for Pie Chart
@@ -29,7 +24,6 @@ def write():
     
 
     #-----------------Markdown info-----------------
-    
     st.markdown('''
     <!-- https://www.w3schools.com/css/tryit.asp?filename=trycss_tooltip_transition & https://www.w3schools.com/css/tryit.asp?filename=trycss_tooltip_right-->
     <style>
@@ -37,7 +31,6 @@ def write():
             position: relative;
             display: inline-block;
             font-size:1.6rem;
-            
         }
         
         .tooltip .tooltiptext {
@@ -55,11 +48,9 @@ def write():
             z-index: 1;
             top: -5px;
             left: 105%;
-            
             opacity: 0;
             transition: opacity 0.8s;
         }
-        
         .tooltip:hover .tooltiptext {
             visibility: visible;
             opacity: 1;
@@ -69,41 +60,52 @@ def write():
 
     st.markdown('''
         <div class="tooltip">&#x24D8
-        <span class="tooltiptext">
-        <b>Pie Chart</b><br>
-        The pie chart represents the age distribution worldwide for the selected year.
-        <br><br>
-        <b>Sankey Diagram</b><br>
-        The Sankey diagram shows the distribution of asylum applications from the different countries of origin (left) to the different countries of destination (right).
-        Top 10 destination countries of a year are illustrated here.
-        <br><br>
-        It should be noted that due to the overview, unknown data as well as data on overseas countries and territories have been removed from the dataset.  In addition, for a few countries only temporary data has been provided.
-        </span></div>
+            <span class="tooltiptext">
+                <b>Pie Chart</b><br>
+                    The pie chart shows the age distribution worldwide for the selected year. 
+                <br><br>
+                <b>Sankey Diagram</b><br>
+                    The Sankey diagram shows the top 10 origin (left) and target (right) countries 
+                    as well as the distribution of asylum seekers per year.
+            </span>
+        </div>
         ''', unsafe_allow_html=True)  
 
+    # read csv
+    # old csv  pie structure 
+    # df = pd.read_csv('https://raw.githubusercontent.com/hannahkruck/VIS_Test1/Develop/piechart.csv',sep = ';')
+    # new csv  pie structure
+    df = pd.read_csv('https://raw.githubusercontent.com/hannahkruck/VIS_Test1/Develop/piechart_neu_struk.csv',sep = ';')
 
+
+    #-----------------Slider-------------------
+    # Slider to choose Year for diagramm
+    year = st.slider("", (int(df["Year"].min())),(int(df["Year"].max())))
+    selected_year = year
+
+
+    #-----------------Page Layout--------------
     # Layout setting of the page 
     c1, c2 = st.beta_columns((1, 1))
     container = st.beta_container()
     st.write('<style>div.Widget.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
+
+    #------------------Create Sankey diagram-------------------------------
+    # https://www.geeksforgeeks.org/sankey-diagram-using-plotly-in-python/
+    # https://coderzcolumn.com/tutorials/data-science/how-to-plot-sankey-diagram-in-python-jupyter-notebook-holoviews-and-plotly#2
     
-#-------------------------Create Sankey diagram-------------------------------
-#https://www.geeksforgeeks.org/sankey-diagram-using-plotly-in-python/
-#https://coderzcolumn.com/tutorials/data-science/how-to-plot-sankey-diagram-in-python-jupyter-notebook-holoviews-and-plotly#2
-    
-    # Variabel fuer Sankey diagramm
-    yearVar = 2019                       
-    
-    #daten einlesen & selectieren
+    # Variable year for Sankey diagram
+    yearVar = selected_year    
+                         
+    # read sankey csv and data selection
     show_df = pd.read_csv('https://raw.githubusercontent.com/hannahkruck/VIS_Test1/Develop/Datensatz_Sankey_Diagramm_eng.csv',sep = ';')
 
-    #YEAR
+    # year
     yearRows = show_df[show_df['Year'] != yearVar].index
     show_df.drop(yearRows , inplace=True)
 
-
-    # Nodes & links & colors
+    # Nodes, links, colors
     label_souce = show_df['Label_Source'].dropna(axis=0, how='any')
     label_souce2 = []
     elementVar = ''
@@ -119,11 +121,11 @@ def write():
     target = show_df['Target'].dropna(axis=0, how='any')
     value = show_df['Value'].dropna(axis=0, how='any')
 
-        #color
+    # setting color for node and link
     color_node = [
-    #Source Syria, Afghanistan, Venezuela, Irak, Colombia, Pakistan, Türkei, Nigeria, Iran, Albania
+    # Source order Syria, Afghanistan, Venezuela, Irak, Colombia, Pakistan, Türkei, Nigeria, Iran, Albania
     '#40bf77', '#93beec', '#1ff91f', '#cd8162', '#a6a6a6', '#80e5ff', '#b299e6', '#ff33ff', '#CDC037', '#ff6a6a',
-    #Target 
+    # Target 
     '#0B2641', '#0B2641', '#0B2641', '#0B2641', '#0B2641', '#0B2641', '#0B2641', '#0B2641', '#0B2641', '#0B2641']
     
     color_link = [
@@ -139,40 +141,77 @@ def write():
     '#ffcccc', '#ffcccc', '#ffcccc', '#ffcccc', '#ffcccc', '#ffcccc', '#ffcccc', '#ffcccc', '#ffcccc', '#ffcccc',]  
 
     # data to dict, dict to sankey
-    link = dict(source = source, target = target, value = value, color=color_link)
-    node = dict(label = label, pad= 20, thickness=10, color=color_node)
-
+    link = dict(source = source, target = target, value = value, color = color_link)
+    node = dict(label = label, pad = 20, thickness = 10, color = color_node)
     layout = dict(
-            #"Top 10 Verteilung der Asylanträge eines Landes auf die verschiedenen Zielländer"
-            #title= 'Top 10 Distribution of a Countries Asylum Applications among the various <br>Countries of Destination  %s' % yearVar,
-            height = 800,                   
-            font = dict(size = 11),)
+        height = 800, 
+        font = dict(
+            size = 11),)
+    
     data = go.Sankey(link = link, node=node)
     
-    # Eigenschaften Sanky Diagram Layout 
+    # properties sanky diagram layout 
     fig2 = go.Figure(data, layout= layout)
 
-#------------Create pie chart-------------------
-    # Transfer data to list
+    #-----------------Create pie chart-------------------
+    # https://jasonxqh.github.io/2020/07/12/plotly基础/
     
+    # read csv for pie chart
+    df = pd.read_csv('https://raw.githubusercontent.com/hannahkruck/VIS_Test1/Develop/piechart.csv',sep = ';')
+	
+    # show specific value if a choosen year is selected on slider
     labels = df['year'].tolist()
-    values = df['2019'].tolist()
-    layout = dict( 
-        height = 600,       
-        font = dict(size = 11)            
-        #title='Age Distribution of Asylum Seekers Worldwide %s'
-        )
-    data = go.Pie(labels=labels, values=values)
+    if selected_year == 2010:
+            values = df['2010'].tolist()
+    if selected_year == 2011:
+            values = df['2011'].tolist()     
+    if selected_year == 2012:
+            values = df['2012'].tolist()       
+    if selected_year == 2013:
+            values = df['2013'].tolist()
+    if selected_year == 2014:
+            values = df['2014'].tolist()
+    if selected_year == 2015:
+            values = df['2015'].tolist()
+    if selected_year == 2016:
+            values = df['2016'].tolist()
+    if selected_year == 2017:
+            values = df['2017'].tolist()
+    if selected_year == 2018:
+            values = df['2018'].tolist()
+    if selected_year == 2019:
+            values = df['2019'].tolist()
+            
+    # define color sets 
+    colors = ['#e6f2ff', '#b3d9ff', '#80bfff', '#3386E6']
 
-    # Create pie figure
+    # create pie figure
     fig1 = go.Figure(data=[go.Pie(
-        labels=labels, 
-        values=values, 
-        textinfo='label+percent', 
-        insidetextorientation='radial',)])
-    
-    # Features Pie Diagram Layout
-    fig1 = go.Figure(data, layout=layout)
+            labels = labels, 
+            values = values, 
+            insidetextorientation = 'radial',
+            hole = 0.399,)])
+
+    # update settings hover and text informations
+    fig1.update_traces(
+            hoverinfo = 'label+percent+value', 
+            textinfo = 'percent+label',
+            textfont_size = 11,
+            marker = dict(colors = colors, 
+            line = dict(color = 'lightskyblue', width = 0.1)))  
+	
+    # update settings layout
+    fig1.update_layout(dict(
+        height = 600, 
+        font = dict(
+            size = 12)))
+
+    # add annotations in the center of the donut pie
+    fig1.update_layout(
+    annotations=[dict(
+            text='<b>Age<br> Distribution</b>', 
+            font_size=12, 
+            showarrow=False),])
 
 #------------Create Timeline Years V. 2.0-------------------
     # read CSV for the histogram graph
@@ -202,11 +241,6 @@ def write():
     )
 )
 #------------Create Slider Years V. 2.0-------------------
-    year = st.slider("", (int(df["year"].min())),(int(df["year"].max())))
-    selected_year = year
-    # Delete all cells, except one year (both maps)
-    indexNames = df[ df['year'] != selected_year ].index
-    df.drop(indexNames , inplace=True)
 
     with c1:
         st.subheader('Asylum seekers by age in Europe in the year %s' % selected_year) 
@@ -216,7 +250,7 @@ def write():
         st.plotly_chart(fig2, use_container_width=True)
     with container:
         st.plotly_chart(fig3, use_container_width=True)
-
-
+      
 if __name__ == "__main__":
     write()
+    
