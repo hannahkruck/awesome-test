5f1206240d5da2855fe7de37bb421d4ca135a69f
hannahkruck <73877879+hannahkruck@users.noreply.github.com>
2020-12-29 17:22:02 +0100

    Update resources.py

--- src/pages/resources.py
+++ src/pages/resources.py
@@ -6,39 +6,103 @@ import streamlit as st
 import awesome_streamlit as ast
 from awesome_streamlit.core.services import resources
 
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+import numpy as np
+
 logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
 
 
 def write():
-    """Writes content to the app"""
-    ast.shared.components.title_awesome("Resources")
-    st.sidebar.title("Resources")
-    tags = ast.shared.components.multiselect(
-        "Select Tag(s)", options=ast.database.TAGS, default=[]
-    )
+	
+#ast.shared.components.title_awesome("Welcome")      # Titel Awesome_Streamlit ausblenden
+	
+# CSS Startseite
+	st.markdown("""
+	<style>
+	body {
+		color: black;
+		font-color: black;
+		background-color: white;
+	}
+	h3 {
+		color: #1878a1;
+	}
+	</style>
+    """, unsafe_allow_html=True)
 
-    author_all = ast.shared.models.Author(name="All", url="")
-    author = st.selectbox("Select Author", options=[author_all] + ast.database.AUTHORS)
-    if author == author_all:
-        author = None
-    show_awesome_resources_only = st.checkbox("Show Awesome Resources Only", value=True)
-    if not tags:
-        st.info(
-            """Please note that **we list each resource under a most important tag only!**"""
-        )
-    resource_section = st.empty()
+	st.title("Willkommen bei (TOOLNAME)")
 
-    with st.spinner("Loading resources ..."):
-        markdown = resources.get_resources_markdown(
-            tags, author, show_awesome_resources_only
-        )
-        resource_section.markdown(markdown)
+# HTML Inhalt
+	html = """
+	<h3>"Toolname" ist ein Visualisierungstool ..." <br>
+	Ziel<br>
+	Was kann Tool alles zeigen<br>
+	Welche Optionen haben Nutzer</h3>
+	
+	<hr>
+	<br>
+	"""
+	st.markdown(html, unsafe_allow_html=True)
+	
+	#Expander - Wenn unter titel dann muss es ueber Spalten erstellen stehen
+	my_expander_one = st.beta_expander("Where does the data come from?", expanded=False)
+	
+	with my_expander_one:
+		html_one = """ 
+			<p> Die Daten stammen aus der ... </p>
+			Datensatz: <a target="_blank" href="https://appsso.eurostat.ec.europa.eu/nui/show.do?dataset=migr_asyappctza&lang=de">Asylbewerber und erstmalige Asylbewerber nach Staatsangehörigkeit, Alter und Geschlecht - jährliche aggregierte Daten (gerundet)<br></a>
+			<br>"""
+		st.markdown(html_one, unsafe_allow_html=True)
 
-    if st.sidebar.checkbox("Show Resource JSON"):
-        st.subheader("Source JSON")
-        st.write(ast.database.RESOURCES)
 
-    tags = None
+	
+	#Expander - Wenn unter titel dann muss es ueber Spalten erstellen stehen
+	my_expander_two = st.beta_expander("Implementation of the visualisation tool", expanded=False)
+
+	with my_expander_two:
+		html_two = """ 
+			<p> The visualisation tool was implemented with the open source framework <a target="_blank" href="https://www.streamlit.io"><b> Streamlit</b></a>.<br> 
+			Streamlit is both a library and a framework for Python. 
+			It allows users to create and publish interactive web apps with a graphical user interface and data visualisation. 
+			<br><br>The advantage of Streamlit is that no front-end experience is required and we can create interactive graphical user interfaces and visualisations using only the Python programming language. Probably the strongest point in choosing Streamlit is the time aspect. Because with Streamlit we can invest more time in processing and visualising the data than in dealing with the front end. <br>
+			<br>Furthermore, it is possible to integrate various libraries in Streamlit in order to create diagrams.
+			For the visualisation of our diagrams and filter options, libraries such as Streamlit, Plotly and Pandas were used:
+			<br>
+				<ul>
+					<li>Choropleth and Line Maps</li>
+				</ul>
+			</p>"""
+			
+		st.markdown(html_two, unsafe_allow_html=True)
+
+
+	#Expander - Wenn unter titel dann muss es ueber Spalten erstellen stehen
+	my_expander_three = st.beta_expander("Expand3", expanded=False)
+	with my_expander_three:
+		html_three = """ 
+			<p> Expand3</p>"""
+		st.markdown(html_three, unsafe_allow_html=True)
+
+	#Expander - Wenn unter titel dann muss es ueber Spalten erstellen stehen
+	my_expander_four = st.beta_expander("Expander 4", expanded=False)
+	with my_expander_four:
+		html_four = """ 
+			<p> Inhalt </p>"""
+		st.markdown(html_four, unsafe_allow_html=True)
+
+	#Expander - Wenn unter titel dann muss es ueber Spalten erstellen stehen
+	my_expander_five = st.beta_expander("About us", expanded=False)
+	with my_expander_five:
+		html_five = """ 
+			<p> Inhalt </p>"""
+		st.markdown(html_five, unsafe_allow_html=True)
+		
+	
+	c1, c2 = st.beta_columns((1,1))
+	container = st.beta_container()
+	st.write('<style>div.Widget.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
